baecd6005de4e37f3fc763136bb6021ee253c2d6
Mike Greb <michael@thegrebs.com>
2018-06-30 21:38:29 -0400

    0.27

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Net::Abuse::Utils - Routines useful for processing network abuse
 
 # VERSION
 
-version 0.26
+version 0.27
 
 # SYNOPSIS
 
@@ -152,6 +152,18 @@ this module's distribution.
 # COPYRIGHT AND LICENSE
 
 This software is copyright (c) 2013 by Mike Greb.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+# AUTHORS
+
+- mikegrb <michael@thegrebs.com>
+- Wes Young <github@barely3am.com>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013-2018 by Michael Greb
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
--- lib/Net/Abuse/Utils.pm
+++ lib/Net/Abuse/Utils.pm
@@ -22,7 +22,7 @@ our %EXPORT_TAGS = ( 'all' => [ qw(
 ) ] );
 
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-our $VERSION = '0.26';
+our $VERSION = '0.27';
 $VERSION = eval $VERSION;
 
 # memoize('_return_rr');
@@ -340,6 +340,14 @@ sub get_domain {
 
 =encoding UTF-8
 
+=head1 NAME
+
+Net::Abuse::Utils - Routines useful for processing network abuse
+
+=head1 VERSION
+
+version 0.27
+
 =head1 SYNOPSIS
 
     use Net::Abuse::Utils qw( :all );
@@ -515,6 +523,26 @@ Wes Young <github@barely3am.com>
 
 This software is copyright (c) 2013-2018 by Michael Greb
 
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 AUTHORS
+
+=over 4
+
+=item *
+
+mikegrb <michael@thegrebs.com>
+
+=item *
+
+Wes Young <github@barely3am.com>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by =over 4.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
